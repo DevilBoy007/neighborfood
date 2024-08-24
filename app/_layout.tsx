4ef124eccee7434eadc5202d9a8046ce3b958636
DevilBoy007 <dylan_bakr@comcast.net>
2024-08-24 01:10:22 -0400

    implement welcome screen in the `_layout`
    doing this so that we can actually navigate to it

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import LoginScreen from './welcome';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
@@ -30,6 +31,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
+        <Stack.Screen name="welcome" options={{ headerShown: false }}/>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
