ef271fc3f3d8c31fdec916ac4570fa362d70cd93
DevilBoy007 <dylan_bakr@comcast.net>
2024-08-24 19:56:14 -0400

    add placeholder functionality for button presses
    doing this to show it can do something

--- app/index.tsx
+++ app/index.tsx
@@ -5,11 +5,11 @@ const LoginScreen = () => {
     <View style={[styles.container, Platform.OS === 'web' && styles.containerWeb]}>
       <Text style={[styles.title, Platform.OS === 'web' && styles.titleWeb]}>neighborfood</Text>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button} activeOpacity={0.75}>
+        <TouchableOpacity style={styles.button} activeOpacity={0.75} onPress={() => alert('login')}>
           <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button} activeOpacity={0.75}>
-          <Text style={styles.buttonText}>Register</Text>
+        <TouchableOpacity style={styles.button} activeOpacity={0.75} onPress={() => alert('sign up')}>
+          <Text style={styles.buttonText}>Sign Up</Text>
         </TouchableOpacity>
       </View>
     </View>
