ec202de2ad49b92d260191dfcfe8c8c64a687ff6
DevilBoy007 <dylan_bakr@comcast.net>
2024-08-24 19:52:15 -0400

    separate web & mobile styles
    doing this so that the experience is better for each platform

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,14 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Platform } from 'react-native';
 
 const LoginScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>neighborfood</Text>
+    <View style={[styles.container, Platform.OS === 'web' && styles.containerWeb]}>
+      <Text style={[styles.title, Platform.OS === 'web' && styles.titleWeb]}>neighborfood</Text>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} activeOpacity={0.75}>
           <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} activeOpacity={0.75}>
           <Text style={styles.buttonText}>Register</Text>
         </TouchableOpacity>
       </View>
@@ -20,25 +20,40 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#B7FFB0',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 40,
+    paddingHorizontal: 20,
+  },
+  containerWeb: {
     justifyContent: 'center',
     alignItems: 'center',
-    gap: 20,
+    flexDirection: 'column',
   },
   title: {
     fontSize: 40,
     fontWeight: 'bold',
-    marginBottom: 20,
     fontFamily: 'TitanOne',
     color: '#fff',
+    marginTop: 20,
+  },
+  titleWeb: {
+    textAlign: 'center',
+    fontSize: 60,
   },
   buttonContainer: {
     flexDirection: 'row',
-    gap: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
   },
   button: {
     backgroundColor: '#00bfff',
     padding: 10,
     borderRadius: 5,
+    marginHorizontal: 5,
+    width: 100,
+    marginTop: 20,
   },
   buttonText: {
     color: 'white',
