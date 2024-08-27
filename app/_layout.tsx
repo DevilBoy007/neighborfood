56825683b6f6224749e4db3db30609ec1ef9d9b5
DevilBoy007 <dylan_bakr@comcast.net>
2024-08-27 17:11:49 -0400

    add market path to root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -23,7 +23,8 @@ export default function RootLayout() {
   }
   return (
     <Stack>
-      <Stack.Screen name="index" options={{headerShown: false}}/>
+      <Stack.Screen name="index" options={{headerShown: false}} />
+      <Stack.Screen name="Market" options={{headerShown: false}}/>
     </Stack>
   );
 }
