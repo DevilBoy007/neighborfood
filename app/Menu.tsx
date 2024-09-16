d5f1ec3067facd2313f5de24a29dfaec41f9b11d
DevilBoy007 <dylan_bakr@comcast.net>
2024-09-16 09:14:38 -0400

    add border radius to community needs callout box

--- app/Menu.tsx
+++ app/Menu.tsx
@@ -138,6 +138,7 @@ const styles = StyleSheet.create({
             ios: {
                 borderColor: 'black',
                 borderWidth: 1,
+                borderRadius: 3,
             },
             web: {
                 marginBottom: 0,
