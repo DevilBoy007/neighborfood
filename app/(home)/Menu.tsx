baf3aa235574517a7e0ad77a4bb045dc09a6f574
DevilBoy007 <dylan_bakr@comcast.net>
2024-10-20 23:34:53 -0400

    add route navigation
    doing this to enable order history screen

--- app/(home)/Menu.tsx
+++ app/(home)/Menu.tsx
@@ -18,10 +18,10 @@ import bellPepperImage from '../../assets/images/bellPeppers.jpeg';
 
 const MenuScreen = () => {
     const router = useRouter();
-    const MenuButton = ({ icon, title }: { icon: ImageSourcePropType | string, title: string }) => {
+    const MenuButton = ({ icon, title, destination }: { icon: ImageSourcePropType | string, title: string, destination: string | null }) => {
         if (typeof icon !== 'string') {
             return (
-                <TouchableOpacity style={styles.menuButton}>
+                <TouchableOpacity style={styles.menuButton} onPress={() => router.navigate(`/(home)/${destination}`)} >
                     <Image source={icon} style={styles.icon} />
                     <Text style={styles.menuButtonText}>{title}</Text>
                 </TouchableOpacity>
@@ -70,7 +70,7 @@ const MenuScreen = () => {
                     <MenuButton icon="+" title="add item" />
                     <MenuButton icon={shopIcon} title="manage shops" />
                     <MenuButton icon={dashboardIcon} title="dashboard" />
-                    <MenuButton icon={receiptIcon} title="order history" />
+                    <MenuButton icon={receiptIcon} title="order history" destination={"OrderHistory"}/>
                     <MenuButton icon={contactUsIcon} title="contact us" />
                     <MenuButton icon="!" title="report a problem" />
                 </View>
