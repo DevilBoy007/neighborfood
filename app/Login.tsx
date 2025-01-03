0741894c5f967482d185007ae27e3c4ed7480c0d
DevilBoy007 <dylan_bakr@comcast.net>
2025-01-03 00:19:31 -0500

    create login functionality
    doing this to allow users to authenticate
    (needs styling)

--- app/Login.tsx
+++ app/Login.tsx
@@ -19,9 +19,133 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import firebaseService from '@/handlers/firebaseService';
 
 const LoginScreen = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [user, setUser] = useState<User | null>(null);
+    const router = useRouter();
+
+    useEffect(() => {
+        const checkUser = async () => {
+            try {
+                const userData = await AsyncStorage.getItem('userData');
+                if (userData) {
+                    const DATA = JSON.parse(userData);
+                    setUser(DATA);
+                    console.log('Loaded user data:\n', user);
+                    router.replace('/Market');
+                }
+            } catch (error) {
+                console.error('User not logged in:', error);
+            }
+        };
+        //checkUser();
+    }, []);
+
+    const handleLogin = async () => {
+        try {
+            await firebaseService.connect()
+            const userCredential = await firebaseService.login(email, password);
+            if (userCredential) {
+                const userData = { userCredential };
+                await AsyncStorage.setItem('userData', JSON.stringify(userData));
+                setUser(userData);
+                console.log('User logged in:', userData);
+                router.replace('/Market');
+            }
+        } catch (error) {
+            console.error('Error logging in:', error);
+            setError(error.message);
+        }
+    };
+
+    return (
+        <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            style={styles.container}
+        >
+            <ScrollView scrollEnabled={false}>
+                <Text style={styles.title}>Login</Text>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                    style={styles.input}
+                    placeholder="Email"
+                    value={email}
+                    onChangeText={setEmail}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Password"
+                    value={password}
+                    onChangeText={setPassword}
+                    secureTextEntry
+                />
+                </View>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={handleLogin}
+                    >
+                        <Text style={styles.buttonText}>Login</Text>
+                    </TouchableOpacity>
+                </View>
+                <Text style={styles.errorText}>{error}</Text>
+            </ScrollView>
+            <KeyboardToolbar />
+        </KeyboardAvoidingView>
+    );
 
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#B7FFB0',
+    },
+    title: {
+        fontFamily: 'TitanOne',
+        fontSize: 36,
+        fontWeight: 'bold',
+        color: '#fff',
+        marginTop: 33,
+        textAlign: 'center',
+        paddingBottom: 16,
+    },
+    inputContainer: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    input: {
+        width: '80%',
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    button: {
+        width: '100%',
+        height: 40,
+        margin: 12,
+        padding: 10,
+        backgroundColor: '#00bfff',
+        alignSelf: 'stretch',
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 16,
+        fontFamily: 'TextMeOne',
+    },
+    errorText: {
+        color: 'red',
+        marginVertical: 2,
+        marginHorizontal: 2,
+        fontFamily: 'TextMeOne',
+    }
+})
 
 export default LoginScreen;
--- handlers/firebaseService.tsx
+++ handlers/firebaseService.tsx
@@ -1,5 +1,5 @@
 import { initializeApp, FirebaseApp } from 'firebase/app';
-import { initializeAuth, getReactNativePersistence, createUserWithEmailAndPassword, Auth, UserCredential } from 'firebase/auth';
+import { initializeAuth, getReactNativePersistence, createUserWithEmailAndPassword, signInWithEmailAndPassword, Auth, UserCredential } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import {
     getFirestore,
@@ -63,6 +63,29 @@ class FirebaseService {
                 throw error;
             }
         } 
+    }
+
+    async login(email: string, password: string): Promise<UserCredential> {
+        try {
+            if (!this.auth) {
+                throw new Error('Firebase Auth is not initialized');
+            }
+            const userCredential = await signInWithEmailAndPassword(this.auth, email, password);
+            console.log('User logged in:', userCredential.user.uid);
+            return userCredential;
+        } catch (error) {
+            console.error('Error logging in:', error);
+            switch (error.code) {
+                case 'auth/invalid-email':
+                    throw new Error('Invalid email');
+                case 'auth/user-not-found':
+                    throw new Error('User not found');
+                case 'auth/wrong-password':
+                    throw new Error('Wrong password');
+                default:
+                    throw error;
+            }
+        }
     }
 
     async registerUser(email: string, password: string, username: string): Promise<UserCredential['user']> {
