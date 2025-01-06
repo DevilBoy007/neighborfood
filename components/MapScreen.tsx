361962aa8b615b528ad614dca7ec84ade7d0866a
DevilBoy007 <dylan_bakr@comcast.net>
2025-01-06 14:07:35 -0500

    remove mapbox references
    doing this because google seems more prevalent

--- components/MapScreen.tsx
+++ components/MapScreen.tsx
@@ -2,36 +2,23 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import * as Location from 'expo-location';
-import 'mapbox-gl/src/css/mapbox-gl.css';
-import MapboxGL from '@rnmapbox/maps';
 
-MapboxGL.setAccessToken('pk.eyJ1IjoiZGJha3IiLCJhIjoiY20wa2llMWplMTloNTJqcTBpcTBoMGZhMiJ9.cr0XwiK22YtOzeSP2bcKwA');
-const [location, setLocation] = useState<Location.LocationObject | null>(null);
+const [location, setLocation] = useState<Location.LocationObjectCoords | null>(null);
 const [errorMsg, setErrorMsg] = useState<string | null>(null);
 
 useEffect(() => {
     (async () => {
-
-        let { status } = await Location.requestForegroundPermissionsAsync();
-        if (status !== 'granted') {
-            setErrorMsg('Permission to access location was denied');
-            return;
-        }
-
-        let location = await Location.getCurrentPositionAsync({});
-        setLocation(location);
+        let location = await Location.getCurrentPositionAsync({
+            accuracy: Location.Accuracy.High,
+        });
+        setLocation(location.coords);
     })();
 }, []);
 
 const MapScreen = () => {
     return (
         <View style={styles.container}>
-            <MapboxGL.MapView style={styles.map}>
-                <MapboxGL.Camera
-                    zoomLevel={14}
-                    centerCoordinate={location ? [location.coords.longitude, location.coords.latitude] : [-122.4324, 37.78825]}
-                />
-            </MapboxGL.MapView>
+            
         </View>
     );
 };
