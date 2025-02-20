d8f8997692cb3489899f026b121bedc1b52e3513
DevilBoy007 <dylan_bakr@comcast.net>
2025-02-20 00:16:55 -0500

    implement AddShop screen
    doing this to allow user to navigate to it within the (shops) route

--- app/(home)/(shops)/_layout.tsx
+++ app/(home)/(shops)/_layout.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Stack } from "expo-router";
 import * as SplashScreen from 'expo-splash-screen';
+import { KeyboardProvider } from 'react-native-keyboard-controller';
+
 
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
     return (
-        <>
+        <KeyboardProvider>
             <Stack.Screen options={{ headerShown: false }} />
             <Stack>
                 <Stack.Screen name="Shops" options={{ headerShown: false}} />
+                <Stack.Screen name="AddShop" options={{ headerShown: false }} />
             </Stack>
-        </>
+        </KeyboardProvider>
     );
 }
