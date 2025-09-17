e024b1b4969f32439314b00baac0acb66d7fa7f7
DevilBoy007 <dylan_bakr@comcast.net>
2025-09-16 21:36:16 -0400

    add tapPlayer and reduce volume
    to allow tap sound to be used and to enhance UX with softer sounds

--- hooks/useSound.ts
+++ hooks/useSound.ts
@@ -1,19 +1,33 @@
-import { AudioPlayer, useAudioPlayer } from 'expo-audio';
-import { useEffect } from 'react';
+import { useAudioPlayer } from 'expo-audio';
 
 import clickSound from '@/assets/sounds/click.mp3';
+import tapSound from '@/assets/sounds/tap.wav';
 
 export const useSound = () => {
-  const player = useAudioPlayer(clickSound);
+
+  const clickPlayer = useAudioPlayer(clickSound);
+  const tapPlayer = useAudioPlayer(tapSound);
+
+  clickPlayer.volume = 0.5;
+  tapPlayer.volume = 0.1;
 
   const playClickSound = () => {
     try {
-      player.seekTo(0);
-      player.play();
+      clickPlayer.seekTo(0);
+      clickPlayer.play();
     } catch (error) {
       console.error('Error playing sound:', error);
     }
   };
 
-  return { playTapSound: playClickSound };
+  const playTapSound = () => {
+    try {
+      tapPlayer.seekTo(0);
+      tapPlayer.play();
+    } catch (error) {
+      console.error('Error playing sound:', error);
+    }
+  };
+
+  return { playTapSound, playClickSound };
 };
